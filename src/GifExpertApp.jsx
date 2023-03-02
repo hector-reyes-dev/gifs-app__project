8f1514a5d9240d78818e740c0e7756f1151b4f3e
Hector <hectordevx@gmail.com>
2023-03-02 17:31:59 -0600

    feat(components): Styles and onAddCategory event

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch"]);
+
+  const onAddCategory = () => {
+    setCategories([...categories, "Halo"]);
+  };
+
   return (
     <>
       <h1>GifExpertApp</h1>
+      <button onClick={onAddCategory}>Agregar</button>
+      <ol>
+        {categories.map((category) => (
+          <li key={category}>{category}</li>
+        ))}
+      </ol>
     </>
   );
 };
